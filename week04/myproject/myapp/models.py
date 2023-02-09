bddd16b5d91fb557e9925711a2eb9056ff8995c5
Wasit Limprasert <wasit7@gmail.com>
2023-02-09 22:15:30 +0700

    update week04

--- week04/myproject/myapp/models.py
+++ week04/myproject/myapp/models.py
@@ -4,6 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     published_date = models.DateField()
+    status = models.CharField(max_length=10 ,default= 'ready')
 
     def __str__(self):
         return f"{self.author}, {self.title}, {self.published_date}"
