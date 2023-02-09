b282ac83692ce359e956728d272c746e6ecbd05d
Wasit Limprasert <wasit7@gmail.com>
2023-02-09 22:14:48 +0700

    after week04

--- week04/myproject/myapp/admin.py
+++ week04/myproject/myapp/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id','title', 'author', 'published_date')
+    list_display = ('id','status','title', 'author', 'published_date')
     list_display_links = ('id',)
     list_editable = ('title',)
     list_filter = ('author',)
     search_fields =('title',)
+    actions = ['make_published','make_soldout']
+    
+    def make_published(self, request, queryset):
+        queryset.update(status='published')
+    
+    def make_soldout(self, request, queryset):
+        queryset.update(status='soldout')
 
 admin.site.register(Book, BookAdmin)
