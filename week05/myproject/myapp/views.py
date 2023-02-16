da51ea7ce6e14158bfbd9858708e2b120f20e6d6
Wasit Limprasert <wasit7@gmail.com>
2023-02-16 20:30:20 +0700

    update lesson4

--- week05/myproject/myapp/views.py
+++ week05/myproject/myapp/views.py
@@ -4,7 +4,9 @@ from .forms import ContactForm
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("""Hello, World!<br> Please contact us click <a href="contact">here</a>""")
+    return HttpResponse(
+        """Please contact us click <a href="contact">here</a>"""
+        )
 
 def contact_view(request): 
    if request.method == "POST": 
@@ -12,9 +14,9 @@ def contact_view(request):
        if form.is_valid(): 
            print('email:', form.cleaned_data['email']) 
            print('message:', form.cleaned_data['message']) 
-           # we can use cleaned_data for other task e.g. send an email  
+           # we can use cleaned_data for other task
+           # e.g. send an email  
            return redirect('home')
    else: 
        form = ContactForm() 
-
    return render(request, "contact.html", {'form':form})
