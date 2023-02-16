eb2ba88a2e348582373d9084b650afa4da1adffb
Wasit Limprasert <wasit7@gmail.com>
2023-02-16 11:32:57 +0700

    update template

--- week05/myproject/myapp/views.py
+++ week05/myproject/myapp/views.py
@@ -1,12 +1,11 @@
 #views.py
 from django.http.response import HttpResponseRedirect 
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from .forms import ContactForm 
-
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("""Hello, World!<br> Please contact us click <a href="contact">here</a>""")
 
 def contact_us_view(request): 
    if request.method == "POST": 
@@ -15,7 +14,7 @@ def contact_us_view(request):
            print('email:', form.cleaned_data['email']) 
            print('message:', form.cleaned_data['message']) 
            # do whatever you want to do, for example, send an email  
-           return HttpResponseRedirect('/') 
+           return redirect('home')
    else: 
        form = ContactForm() 
 
--- week05/myproject/myproject/urls.py
+++ week05/myproject/myproject/urls.py
@@ -20,5 +20,5 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('form-without-model/', views.contact_us_view,name='contact')
+    path('contact/', views.contact_us_view,name='contact')
 ]
