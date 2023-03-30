899cddfdb37a432084e0642ceec828b0947e4470
Wasit Limprasert <wasit7@gmail.com>
2023-03-30 12:13:42 +0700

    detail view

--- lessson06 ClassbasedView/myproject/sale/views.py
+++ lessson06 ClassbasedView/myproject/sale/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Product
 
-# Create your views here.
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
