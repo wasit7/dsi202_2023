b18a6297cc9ac543ec5f0571b94f6bb6f1707a5b
Wasit Limprasert <wasit7@gmail.com>
2023-04-27 16:06:25 +0700

    update

--- final_project/myproject/myproject/settings.py
+++ final_project/myproject/myproject/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
     'sale',
-    # 'extra_views',
     'django_extensions',
 ]
 
--- final_project/myproject/sale/urls.py
+++ final_project/myproject/sale/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('products/', ProductListView.as_view(), name='product_list'),
     path('add_to_cart/<int:product_id>', add_to_cart, name='add_to_cart'),
-    # path('item_formset/', ItemFormSetView.as_view(), name='item_formset'),
-    # path('order_summary/', OrderSummaryView.as_view(), name='order_summary'),
     path('order_create/', OrderCreateView.as_view(), name='order_create'),
 ]
