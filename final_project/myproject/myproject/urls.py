90fb700c28e4828cce10b983283289921cb63f66
Wasit Limprasert <wasit7@gmail.com>
2023-04-27 09:22:50 +0700

    init final project

--- final_project/myproject/myproject/urls.py
+++ final_project/myproject/myproject/urls.py
@@ -21,9 +21,3 @@ urlpatterns = [
     path('sale/', include('sale.urls')),
     path('', include('myapp.urls'))
 ]
-
-
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
